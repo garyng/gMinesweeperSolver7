cd9f6ff43a9d7233eaea57544538c1abd97193c0
GaryNg <garyng.zhongbo@gmail.com>
2015-12-27 02:52:14 +0800

    + Minesweeper Auto Solver Core Functions

--- gMinesweeperSolver7/ProcessMemoryReader.cs
+++ gMinesweeperSolver7/ProcessMemoryReader.cs
@@ -9,13 +9,13 @@ namespace gMinesweeperSolver7
 	public class ProcessMemoryReader
 	{
 		#region Constants
-		
+
 		public const uint PROCESS_VM_READ = 0x0010;
 
 		#endregion
 
 		#region APIs
-		
+
 		//		HANDLE OpenProcess(
 		//			DWORD dwDesiredAccess,  // access flag
 		//			BOOL bInheritHandle,    // handle inheritance option
@@ -63,9 +63,9 @@ namespace gMinesweeperSolver7
 
 		#region Public Functions
 
-		public IntPtr OpenProcess()
+		public void OpenProcess()
 		{
-			return openProcess(_pProcess.Id);
+			_hProcess = openProcess(_pProcess.Id);
 		}
 
 		public int CloseHandle()
@@ -81,8 +81,8 @@ namespace gMinesweeperSolver7
 		#endregion
 
 		#region Private Fields
-		
-		private Process _pProcess = null;
+
+		private Process _pProcess;
 		private IntPtr _hProcess = IntPtr.Zero;
 
 		#endregion
@@ -112,8 +112,116 @@ namespace gMinesweeperSolver7
 			bytesRead = ptrBytesRead.ToInt32();
 
 			return buffer;
+		}
+
+		#endregion
+
+	}
+
+	public class ProcessMemoryWriter
+	{
+		#region Constants
+
+		public const int PROCESS_VM_WRITE = 0x0020;
+		public const int PROCESS_VM_OPERATION = 0x0008;
+
+		#endregion
+
+		#region APIs
+
+		//		HANDLE OpenProcess(
+		//			DWORD dwDesiredAccess,  // access flag
+		//			BOOL bInheritHandle,    // handle inheritance option
+		//			DWORD dwProcessId       // process identifier
+		//			);
+		[DllImport("kernel32.dll")]
+		public static extern IntPtr OpenProcess(UInt32 dwDesiredAccess, Int32 bInheritHandle, UInt32 dwProcessId);
+
+		//		BOOL CloseHandle(
+		//			HANDLE hObject   // handle to object
+		//			);
+		[DllImport("kernel32.dll")]
+		public static extern Int32 CloseHandle(IntPtr hObject);
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, UInt32 dwSize, out IntPtr lpNumberOfBytesWritten);
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>	
+		/// Process from which to read		
+		/// </summary>
+		public Process Process
+		{
+			get
+			{
+				return _pProcess;
+			}
+			set
+			{
+				_pProcess = value;
+			}
+		}
+
+		#endregion
+
+		#region Public Functions
+
+		public void OpenProcess()
+		{
+			_hProcess = openProcess(_pProcess.Id);
+		}
+
+		public int CloseHandle()
+		{
+			return closeHandle(_hProcess);
+		}
+
+		public void WriteProcessMemory(IntPtr memoryAddress,byte[] buffer,UInt32 size, out int bytesWritten)
+		{
+			writeProcessMemory(_hProcess,memoryAddress,buffer,size,out bytesWritten);
+		}
+
+		#endregion
+
+		#region Private Fields
+
+		private Process _pProcess;
+		private IntPtr _hProcess = IntPtr.Zero;
+
+		#endregion
+
+		#region Private Functions
+
+		private IntPtr openProcess(int processId)
+		{
+			IntPtr pHandle = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_WRITE , 1, (uint)processId);
+			return pHandle;
+		}
+		private int closeHandle(IntPtr handle)
+		{
+			int iRet = CloseHandle(handle);
+			if (iRet == 0)
+			{
+				throw new Exception("CloseHandle() failed.");
+			}
+			return iRet;
+		}
+		private void writeProcessMemory(IntPtr handle, IntPtr address,byte[] buffer, uint size, out int bytesWritten)
+		{
+			//byte[] buffer = new byte[size];
+			IntPtr ptrBytesWritten;
+
+			//ReadProcessMemory(handle, address, buffer, size, out ptrBytesRead);
+			WriteProcessMemory(handle, address, buffer, size, out ptrBytesWritten);
+
+			bytesWritten = ptrBytesWritten.ToInt32();
 
 		}
+
+
 
 		#endregion
 
